bb5b0d1153a9a50c033a223156f458f478286ecf
ehrhardt98 <jorgeavbe@al.insper.edu.br>
2019-03-26 10:38:05 -0300

    issues resolvidos

--- src/main.c
+++ src/main.c
@@ -21,112 +21,7 @@
 /************************************************************************/
 
 #include "asf.h"
-#define NOTE_B0  31
-#define NOTE_C1  33
-#define NOTE_CS1 35
-#define NOTE_D1  37
-#define NOTE_DS1 39
-#define NOTE_E1  41
-#define NOTE_F1  44
-#define NOTE_FS1 46
-#define NOTE_G1  49
-#define NOTE_GS1 52
-#define NOTE_A1  55
-#define NOTE_AS1 58
-#define NOTE_B1  62
-#define NOTE_C2  65
-#define NOTE_CS2 69
-#define NOTE_D2  73
-#define NOTE_DS2 78
-#define NOTE_E2  82
-#define NOTE_F2  87
-#define NOTE_FS2 93
-#define NOTE_G2  98
-#define NOTE_GS2 104
-#define NOTE_A2  110
-#define NOTE_AS2 117
-#define NOTE_B2  123
-#define NOTE_C3  131
-#define NOTE_CS3 139
-#define NOTE_D3  147
-#define NOTE_DS3 156
-#define NOTE_E3  165
-#define NOTE_F3  175
-#define NOTE_FS3 185
-#define NOTE_G3  196
-#define NOTE_GS3 208
-#define NOTE_A3  220
-#define NOTE_AS3 233
-#define NOTE_B3  247
-#define NOTE_C4  262
-#define NOTE_CS4 277
-#define NOTE_D4  294
-#define NOTE_DS4 311
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_FS4 370
-#define NOTE_G4  392
-#define NOTE_GS4 415
-#define NOTE_A4  440
-#define NOTE_AS4 466
-#define NOTE_B4  494
-#define NOTE_C5  523
-#define NOTE_CS5 554
-#define NOTE_D5  587
-#define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_FS5 740
-#define NOTE_G5  784
-#define NOTE_GS5 831
-#define NOTE_A5  880
-#define NOTE_AS5 932
-#define NOTE_B5  988
-#define NOTE_C6  1047
-#define NOTE_CS6 1109
-#define NOTE_D6  1175
-#define NOTE_DS6 1245
-#define NOTE_E6  1319
-#define NOTE_F6  1397
-#define NOTE_FS6 1480
-#define NOTE_G6  1568
-#define NOTE_GS6 1661
-#define NOTE_A6  1760
-#define NOTE_AS6 1865
-#define NOTE_B6  1976
-#define NOTE_C7  2093
-#define NOTE_CS7 2217
-#define NOTE_D7  2349
-#define NOTE_DS7 2489
-#define NOTE_E7  2637
-#define NOTE_F7  2794
-#define NOTE_FS7 2960
-#define NOTE_G7  3136
-#define NOTE_GS7 3322
-#define NOTE_A7  3520
-#define NOTE_AS7 3729
-#define NOTE_B7  3951
-#define NOTE_C8  4186
-#define NOTE_CS8 4435
-#define NOTE_D8  4699
-#define NOTE_DS8 4978
-#define  a3f    208     // 208 Hz
-#define  b3f    233     // 233 Hz
-#define  b3     247     // 247 Hz
-#define  c4     261     // 261 Hz MIDDLE C
-#define  c4s    277     // 277 Hz
-#define  e4f    311     // 311 Hz
-#define  f4     349     // 349 Hz
-#define  a4f    415     // 415 Hz
-#define  b4f    466     // 466 Hz
-#define  b4     493     // 493 Hz
-#define  c5     523     // 523 Hz
-#define  c5s    554     // 554 Hz
-#define  e5f    622     // 622 Hz
-#define  f5     698     // 698 Hz
-#define  f5s    740     // 740 Hz
-#define  a5f    831     // 831 Hz
-#define rest    -1
+#include "notas.h"
 
 /************************************************************************/
 /* defines                                                              */
@@ -154,10 +49,6 @@
 
 
 /************************************************************************/
-/* constants                                                            */
-/************************************************************************/
-
-/************************************************************************/
 /* variaveis globais                                                    */
 /************************************************************************/
 
@@ -171,7 +62,7 @@ volatile int musica;
 void init(void);
 
 /************************************************************************/
-/* interrupcoes                                                         */
+/* callbacks                                                            */
 /************************************************************************/
 
 void but1_callback(void){
@@ -242,6 +133,7 @@ void init(void)
 	NVIC_SetPriority(BUT2_PIO_ID, 4); // Prioridade 4
 }
 
+// função para o buzzer tocar na frequência certa por certo tempo
 void buzz(int freq, int len) {
 	pio_set(BUZ_PIO, BUZ_PIO_IDX_MASK);
 	
@@ -263,10 +155,13 @@ void buzz(int freq, int len) {
 }
 
 /************************************************************************/
-/* Main                                                                 */
+/* Musicas																*/
+/* autoria:																*/
+/* musicas mario: https://www.hackster.io/techarea98/super-mario-theme-song-with-piezo-buzzer-and-arduino-2cc461 */
+/* fur elise: http://repairmypc.net/2017/08/test-post/					*/
 /************************************************************************/
 
-int melody[] = {
+const int melody[] = {
 	NOTE_E7, NOTE_E7, 0, NOTE_E7,
 	0, NOTE_C7, NOTE_E7, 0,
 	NOTE_G7, 0, 0,  0,
@@ -293,7 +188,7 @@ int melody[] = {
 	NOTE_D7, NOTE_B6, 0, 0
 };
 
-int tempo[] = {
+const int tempo[] = {
 	12, 12, 12, 12,
 	12, 12, 12, 12,
 	12, 12, 12, 12,
@@ -320,7 +215,7 @@ int tempo[] = {
 	12, 12, 12, 12
 };
 
-int underworld_melody[] = {
+const int underworld_melody[] = {
 	125,
 	NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
 	NOTE_AS3, NOTE_AS4, 0,
@@ -343,7 +238,7 @@ int underworld_melody[] = {
 	0, 0, 0
 };
 
-int underworld_tempo[] = {
+const int underworld_tempo[] = {
 	125,
 	12, 12, 12, 12,
 	12, 12, 6,
@@ -366,7 +261,7 @@ int underworld_tempo[] = {
 	3, 3, 3
 };
 
-int fur_melody[] = {
+const int fur_melody[] = {
 	NOTE_B4, NOTE_A4, NOTE_GS4, NOTE_A4, NOTE_C5,
 	NOTE_D5, NOTE_C5, NOTE_B4, NOTE_C5, NOTE_E5,
 	NOTE_F5, NOTE_E5, NOTE_DS5, NOTE_E5,
@@ -388,7 +283,7 @@ int fur_melody[] = {
 	NOTE_A5, NOTE_B5, NOTE_CS6, NOTE_A5, NOTE_B5, NOTE_CS6,  NOTE_B5,NOTE_A5,NOTE_GS5,NOTE_FS5, NOTE_B5,NOTE_GS5,NOTE_E5,NOTE_A5,
 	
 };
-int fur_tempo[] = {
+const int fur_tempo[] = {
 	8, 8, 8, 8, 2,
 	8, 8, 8, 8, 2,
 	8, 8, 8, 8,
@@ -420,11 +315,10 @@ int main(void)
 {	
   init();
   flag = false;
-  melody[0] = sizeof(melody) / 4; // / sizeOf(int);
+  melody[0] = sizeof(melody) / sizeof(melody[0]);
   underworld_melody[0] = sizeof(underworld_tempo) / 4;
 
   // super loop
-  // aplicacoes embarcadas não devem sair do while(1).
   while (1)
   {
 	  if(!flag) pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
